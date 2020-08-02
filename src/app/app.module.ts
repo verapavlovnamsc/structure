336327538b6e8a72a027326989612d9ec1ac5354
verapavlovnamsk <cris.glazkowa@yandex.ru>
2020-08-02 14:34:55 +0300

    second commit - add photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContentComponent } from './layout/content/content.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    ContentComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
